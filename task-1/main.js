26942d6f03bdbb8f496d9175cb2070aa9e6c44a9
RustamIsmayilov96 <106178348+RustamIsmayilov96@users.noreply.github.com>
2022-07-05 19:18:17 +0300

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,8 +1,24 @@
-// Use this matrix for solution
-let matrix = [
-    [4, -2, 5],
-    [1, -4, -12],
-    [0, 1, -3]
-]
+const prompt = require('prompt');
+prompt.start();
 
-// Write code here. 
+
+prompt.get('input', function (err, result) {
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Number type-a çevirmək üçün parseInt() metodundan istifadə edirik.
+    Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
+    */
+    var input = result.input; // "5,2 4 7 12 9"
+    var matrix = input.split(','); // ['5', '2 4 7 12 9']
+    let count = 0
+    let sum = 0
+    for (let i = 0; i < matrix.length; i++) {
+       for (let j = 0; j < matrix.length; j++){
+    if(matrix[i][j]<0 && matrix[i][j]%2==0){
+        count++
+        sum = sum + matrix[i][j]
+    }
+    }
+    }console.log(count);
+    console.log(sum);
+}); 
