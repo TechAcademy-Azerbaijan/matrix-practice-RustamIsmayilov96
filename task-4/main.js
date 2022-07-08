791834869d40ddff0c91ea50970dcfbd46d8db0f
RustamIsmayilov96 <106178348+RustamIsmayilov96@users.noreply.github.com>
2022-07-08 13:22:34 +0300

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,5 +1,30 @@
-let n = 5;
-let i = 4;
-let j = 2;
+const n = 5
+const matrix= []
 
-// Write code here. 
+for (let i = 0; i < n; i++) {
+    matrix.push(new Array(n))
+}
+let count = 1
+let j = 0
+let i = 0
+let directionI = 0
+let directionJ = 0
+
+while(count <= n**2){
+    matrix[i][j] == count;
+
+if( j+directionJ === n||
+    i+directionI == n ||
+    j+directionJ == -1||
+    matrix[i+directionI][j+directionJ] != undefined){
+        let temp = directionI
+        directionI = directionJ
+        directionJ =  temp
+    }
+    i= i+ directionI
+    j=j+directionJ
+    count++
+}
+for (let i = 0; i < matrix.length; i++) {
+    console.log(matrix[i].join(" "));
+}
