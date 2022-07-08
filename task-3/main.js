1c104e13d17de180141507103173f5c18cba262e
RustamIsmayilov96 <106178348+RustamIsmayilov96@users.noreply.github.com>
2022-07-08 12:58:05 +0300

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,10 +1,23 @@
-// Use this matrix for solution
-
 let matrix = [
     [2, 5, 3, 1, 4],
     [3, 5, 2, 3, 4],
     [4, 6, 1, 2, 3],
     [4, 5, 6, 7, 7]
 ]
+let index =[]
+let min = matrix[matrix.length -1][matrix.length -1]
+let matriks = []
+let count = 0
 
-// Write code here. 
+
+for (let i = 0; i < matrix.length; i++) {
+           for (let j = 0; j < matrix[i].length; j++){
+if(matrix[i][j]<min){
+    min= matrix[i][j]
+    matriks=[]
+    matriks.push(matrix[i])
+    count++
+console.log(matriks);
+}
+           }
+}
